c1fd4c5bd14b98346be38cf22293ecb8b4f01f8f
astalaramagg <asta.magg94@gmail.com>
2018-11-13 22:30:15 +0000

    Aðeins búin að bæta við

--- TimeChanger.js
+++ TimeChanger.js
@@ -18,6 +18,6 @@ TimeChanger.prototype.update = function(du) {
 
 TimeChanger.prototype.render = function(ctx) {
     //console.log('g_sprites', g_sprites.timeChanger[Math.round(Math.random())]);
-    
-    ctx.drawImage(g_sprites.timeChanger[Math.round(Math.random())].image, 10, 10, 150, 180);
+    //bara draw öðru hvoru og bara annað þá
+    //ctx.drawImage(g_sprites.timeChanger[Math.round(Math.random())].image, 10, 10, 150, 180);
 }
